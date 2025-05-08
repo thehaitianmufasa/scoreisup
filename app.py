f67929dbfe4ae6b28c1f348d1f586c7629d1b7ea
thehaitianmufasa <thejeffchery@gmail.com>
2025-05-08 17:57:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -21,41 +21,41 @@ if st.session_state["user"]:
         st.experimental_rerun()
 
 # --- Not Logged In ---
-else:
-    if st.session_state["login_mode"] == "login":
-        st.subheader("Log In")
-        login_email = st.text_input("Email")
-        login_password = st.text_input("Password", type="password")
-        if st.button("Log In"):
-            user = get_user_by_email(login_email)
-            if user and bcrypt.checkpw(login_password.encode('utf-8'), user[2].encode('utf-8')):
-                st.session_state["user"] = user[1]
-                st.success("Login successful!")
+elif st.session_state["login_mode"] == "login":
+    st.subheader("Log In")
+    login_email = st.text_input("Email")
+    login_password = st.text_input("Password", type="password")
+    if st.button("Log In"):
+        user = get_user_by_email(login_email)
+        if user and bcrypt.checkpw(login_password.encode('utf-8'), user[2].encode('utf-8')):
+            st.session_state["user"] = user[1]
+            st.success("Login successful!")
+            st.experimental_rerun()
+        else:
+            st.error("Invalid email or password.")
+    if st.button("Go to Sign Up"):
+        st.session_state["login_mode"] = "signup"
+        st.experimental_rerun()
+
+# --- Sign Up Flow ---
+elif st.session_state["login_mode"] == "signup":
+    st.subheader("Sign Up")
+    signup_email = st.text_input("Email", key="signup_email")
+    signup_password = st.text_input("Password", type="password", key="signup_password")
+    if st.button("Create Account"):
+        if get_user_by_email(signup_email):
+            st.error("Account already exists.")
+        else:
+            success = insert_user(signup_email, signup_password)
+            if success:
+                st.success("Account created! Please log in.")
+                st.session_state["login_mode"] = "login"
                 st.experimental_rerun()
             else:
-                st.error("Invalid email or password.")
-        if st.button("Go to Sign Up"):
-            st.session_state["login_mode"] = "signup"
-            st.experimental_rerun()
-
-    else:
-        st.subheader("Sign Up")
-        signup_email = st.text_input("Email", key="signup_email")
-        signup_password = st.text_input("Password", type="password", key="signup_password")
-        if st.button("Create Account"):
-            if get_user_by_email(signup_email):
-                st.error("Account already exists.")
-            else:
-                success = insert_user(signup_email, signup_password)
-                if success:
-                    st.success("Account created! Please log in.")
-                    st.session_state["login_mode"] = "login"
-                    st.experimental_rerun()
-                else:
-                    st.error("Error creating account.")
-        if st.button("Go to Login"):
-            st.session_state["login_mode"] = "login"
-            st.experimental_rerun()
+                st.error("Error creating account.")
+    if st.button("Go to Login"):
+        st.session_state["login_mode"] = "login"
+        st.experimental_rerun()
 
 # --- Protected Area ---
 if st.session_state["user"]:
